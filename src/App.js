15e9024013dbbe7892bfbd622ba6b22cda1458bd
bondeveloper <hellobondeveloper@gmail.com>
2021-01-17 17:10:02 +0200

    Added Logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout';
 
-function App() {
-  return (
-    <Layout />
-  );
+class App extends Component {
+  render () {
+    let routes = (
+      <Switch>
+        <Route path='/' component="" />
+        <Route path='/workouts' component="" />
+      </Switch>
+    )
+    return (
+      <Layout>
+        {routes}
+      </Layout>
+    );
+  }
 }
 
 export default App;
--- src/Navigation/NavigationItems/NavigationItems.js
+++ src/Navigation/NavigationItems/NavigationItems.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-// import classes from './NavigationItems.module.css';
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = props => (
-    <ul>
-        <li>home</li>
-        <li>workouts</li>
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link='/' exact>Home</NavigationItem>
+        <NavigationItem link='/workouts'>Workouts</NavigationItem>
     </ul>
 );
 
